ee27cab12eb640278244556869855707ac6e9795
EBoxall <>
2021-05-06 15:47:44 +0000

    finished assignment 1

---
+++ main.py
@@ -0,0 +1,6 @@
+first_name = input("what's your first name ")
+last_name = input("what's your last name or your mother's maiden name? " )
+
+print("Cool name!", "I'll refer to you as: ", last_name, first_name )
+print("Have fun with the other programs!")
+
